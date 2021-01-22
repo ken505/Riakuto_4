e62495efa70236d66f8f2d9801b5ea9911290902
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-22 21:01:55 +0900

    WIP:

--- Riakuto_4/js/main.js
+++ Riakuto_4/js/main.js
@@ -132,7 +132,7 @@ const users = [
   null,
 ];
 
-for (u of users) {
+for (const u of users) {
   const user = u ?? { name: "(Somebody)" };
   const town = user?.address?.town ?? "(Somewhere)";
   console.log(`${user.name} lives in ${town}`);
